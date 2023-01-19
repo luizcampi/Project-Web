164a2f433d5df000aa7a0f62d28b410f84d11d4a
Luiz Carlos <luizcampi11@gmail.com>
2023-01-19 10:57:24 -0300

    Project created

---
+++ src/test/java/com/projectweb/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectweb.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
